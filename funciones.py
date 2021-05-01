4b141136310bc668fc1090c8650315183f746f61
Laura Martinez Sanchez <lmartisa@gmail.com>
2021-05-01 11:27:14 +0200

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -77,6 +77,68 @@ def moving_w(k, img, mask, funct):
       img[idx[0][i], idx[1][i], :] = result[0]
   return img
 
+def otsu_thresh(img, band):
+  """Codigo para extraer de manera automatica un valor threshold.
+  Codigo basado en: https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html
+  """
+  bins_num = np.max(img)
+  # Obtenemos histograma de la imagen
+  hist, bin_edges = np.histogram(img, bins=bins_num)
+  # Normalizamos el histograma
+  hist = np.divide(hist.ravel(), hist.max())
+  # Calculamos el centro de los bins dels histograma
+  bin_mids = (bin_edges[:-1] + bin_edges[1:]) / 2.
+  # iteramos sobre hist para obtener las probablidades y calculamos sus medias correspondientes
+  weight1 = np.cumsum(hist)
+  weight2 = np.cumsum(hist[::-1])[::-1]
+  mean1 = np.cumsum(hist * bin_mids) / weight1
+  mean2 = (np.cumsum((hist * bin_mids)[::-1]) / weight2[::-1])[::-1]
+
+  inter_class_variance = weight1[:-1] * weight2[1:] * (mean1[:-1] - mean2[1:]) ** 2
+  # Maximize the inter_class_variance function val
+  index_of_max_val = np.argmax(inter_class_variance)
+  thresh = bin_mids[:-1][index_of_max_val]
+  print("Otsu's algorithm implementation thresholding result: ", thresh)
+  return thresh
+
+def autocontraste(x,a,b):
+    '''Realiza el autocontraste llevando los puntos a y b a 0 y 225, respectivamente'''
+    if x<a:
+        y=0
+    elif x>b:
+        y=255
+    else:
+        y=round((255/(b-a))*(x-a))
+    return y
+
+def realce(x,A,B):
+    ''' Función para realizar el realce: si A < B = realce sombras y si A > B = realce claros'''
+    if A < 0:
+        A=0
+    if A > 255:
+        A=255
+    if B < 0:
+        B=0
+    if B > 255:
+        B=255
+    if x <= A:
+        y = round(B*x/A)
+    else:
+        y = round(((255-B)*x+(255*(B-A)))/(255-A))
+    return y
+
+def logaritmo(x,alfa=0.5):
+    c = 223/np.log(1+(np.e**alfa-1)*223)
+    y=c*np.log(1+(np.e**alfa-1)*x)
+    return round(y)
+
+def exponencial(x,alfa=10):
+    x=x/266
+    c = 266/((1+alfa)-1)
+    y=c*((1+alfa)**x-1)
+    return y
+
+
 def filtro_prueba(img):
   # gamma = 0.9
   # lookUpTable = np.empty((1,256), np.uint8)
